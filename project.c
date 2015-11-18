c1afb6deb1c65b39c742a9c816a830ed32e70909
alexguffey <guffeyalex@gmail.com>
2015-11-18 13:56:34 -0500

    added names

--- project.c
+++ project.c
@@ -1,14 +1,14 @@
 #include "spimcore.h"
 
 
-/* ALU */
+/* Hassan: ALU */
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
 
 }
 
-/* instruction fetch */
+/* Trevor: instruction fetch */
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
@@ -16,7 +16,7 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 }
 
 
-/* instruction partition */
+/* Trevor: instruction partition */
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
@@ -25,14 +25,14 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 
 
 
-/* instruction decode */
+/* Trevor: instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
 
 }
 
-/* Read Register */
+/* Alex: Read Register */
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
@@ -40,21 +40,21 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 }
 
 
-/* Sign Extend */
+/* Hassan: Sign Extend */
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
 
 }
 
-/* ALU operations */
+/* Hassan: ALU operations */
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
 
 }
 
-/* Read / Write Memory */
+/* Alex: Read / Write Memory */
 /* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
@@ -62,14 +62,14 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 }
 
 
-/* Write Register */
+/* Alex: Write Register */
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
 
 }
 
-/* PC update */
+/* Alex: PC update */
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
