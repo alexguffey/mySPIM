4fd96bf4768ea363ba68680f98313a3eca5912fa
alexguffey <guffeyalex@gmail.com>
2015-11-28 18:26:29 -0500

    finished my functions

--- project.c
+++ project.c
@@ -37,6 +37,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
 
+    // Read the registers addressed by r1 and r2 from Reg, and write the read values to data1 and data2 respectively.
     *data1 = Reg[r1];
     *data2 = Reg[r2];
 }
@@ -61,12 +62,22 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
 
-    if (MemRead == 1) {
-
+    // Write data2 to memory based on index calculated from ALUresult
+    if (MemWrite == 1) {
+        if (ALUresult % 4 == 0) {
+            Mem[ALUresult >> 2] = data2;
+        } else {
+            return 1;
+        }
     }
 
-    if (MemWrite == 1) {
-
+    // Read memory based on index calculated from ALUresult
+    if (MemRead == 1) {
+        if (ALUresult % 4 == 0) {
+            *memdata = Mem[ALUresult >> 2];
+        } else {
+            return 1;
+        }
     }
 
     return 0;
@@ -81,6 +92,21 @@ void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,
 
     if (RegWrite == 1) {
 
+        if (MemtoReg == 1) {
+            if (RegDst == 0) {
+                Reg[r2] = memdata;
+            } else {
+                Reg[r3] = memdata;
+            }
+        }
+
+        if (MemtoReg == 0) {
+            if (RegDst == 0) {
+                Reg[r2] = ALUresult;
+            } else {
+                Reg[r3] = ALUresult;
+            }
+        }
     }
 
 }
@@ -92,5 +118,14 @@ void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char 
 
     *PC += 4;
 
+    // If branch taken : PC + offset.  Figure out the correct offset value.
+    if (Zero == 1 && Branch == 1) {
+        *PC += extended_value << 2;
+    }
+
+    // Jump: Left shift bits of jsec by 2 and use upper 4 bits of PC
+    if (Jump == 1) {
+        *PC = (jsec << 2) | (*PC & 0xf0000000);
+    }
 }
 
